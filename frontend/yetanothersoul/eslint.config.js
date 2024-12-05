48855e2baceb8c6d1b755ee06db036ff8f7d1650
mzubzzz <zubarev.mihail@gmail.com>
2024-12-05 21:30:01 +0300

    create front project

---
+++ frontend/yetanothersoul/eslint.config.js
@@ -0,0 +1,21 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  eslint.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
+  skipFormatting,
+]
